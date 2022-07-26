1a2ace8c4c6ea579824c172cebee414c1cb9df32
Joaquin Alvarez <alvarezdjoaquin@gmail.com>
2022-07-26 19:47:35 -0300

    Agrego menu hamburguesa y modifico seccion de Nuestra Comida

---
+++ javascript/menu-hamb.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+    $('#menu_on').click(function(){ // Al hacer click...
+       $('#wrapper').toggleClass('visible_menu'); // Añadimos o eliminamos la clase 'visible_menu' al body
+     })
+     $('#detail-pizza').click(function(){ // Al hacer click...
+       $('#wrapper').toggleClass('visible_menu'); // Añadimos o eliminamos la clase 'visible_menu' al body
+     })
+     $('#detail-burger').click(function(){ // Al hacer click...
+       $('#wrapper').toggleClass('visible_menu'); // Añadimos o eliminamos la clase 'visible_menu' al body
+     })
+     $('#detail-empanada').click(function(){ // Al hacer click...
+       $('#wrapper').toggleClass('visible_menu'); // Añadimos o eliminamos la clase 'visible_menu' al body
+     })
+     $('#detail-papas').click(function(){ // Al hacer click...
+       $('#wrapper').toggleClass('visible_menu'); // Añadimos o eliminamos la clase 'visible_menu' al body
+     })
+     $('#detail-cajitafeliz').click(function(){ // Al hacer click...
+       $('#wrapper').toggleClass('visible_menu'); // Añadimos o eliminamos la clase 'visible_menu' al body
+     })
+     $('#detail-viandas').click(function(){ // Al hacer click...
+        $('#wrapper').toggleClass('visible_menu'); // Añadimos o eliminamos la clase 'visible_menu' al body
+      })
+    $('#detail-contacto').click(function(){ // Al hacer click...
+        $('#wrapper').toggleClass('visible_menu'); // Añadimos o eliminamos la clase 'visible_menu' al body
+      })
+});
